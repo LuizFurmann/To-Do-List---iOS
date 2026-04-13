9acb3142e26e8024ed7029ff38afffdb73669783
Luiz Furmann <furmannluiz@gmail.com>
2026-04-13 11:59:56 -0300

    salvando dados localmente

--- ToDoList/viewModels/ListViewModel.swift
+++ ToDoList/viewModels/ListViewModel.swift
@@ -11,19 +11,26 @@ import Combine
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "Engenharia de Software", isCompleted: true),
-            ItemModel(title: "Android", isCompleted: true),
-            ItemModel(title: "iOS", isCompleted: false)
-        ]
-        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let saveItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = saveItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -42,6 +49,12 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: {$0.id == item.id}) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodeData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodeData, forKey: itemsKey)
         }
     }
 }
